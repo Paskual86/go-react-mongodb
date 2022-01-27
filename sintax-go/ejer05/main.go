25c172337050211fa7fc70e9f4e4df602bbe4514
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2022-01-27 12:36:19 +0100

    Ciclos

--- sintax-go/ejer05/main.go
+++ sintax-go/ejer05/main.go
@@ -4,6 +4,32 @@ import "fmt"
 
 func main() {
 
+	var i int = 0
+
+	for i < 10 {
+		fmt.Printf("\n Valor de i: %d", i)
+		i++
+	}
+	fmt.Println()
+	fmt.Println("Forma tradicional del FOR")
+
+	for j := 1; j < 10; j++ {
+		fmt.Printf("\n Valor de i: %d", j)
+	}
+
+	for i < 10 {
+		fmt.Printf("\n Valor de i: %d", i)
+
+		if i == 5 {
+			fmt.Printf(" multiplicamos por 2 \n")
+			i *= 2
+			continue // Manda el control al inicio del for.
+		}
+		fmt.Printf("     Paso por aqui \n")
+
+		i++
+	}
+
 	/*for i := 0; i < count; i++ {
 
 	}*/
@@ -18,21 +44,9 @@ func main() {
 			}
 		}*/
 
-	/*var i = 0
-	for i < 10 {
-		fmt.Printf("\n Valor de i: %d", i)
+	/**/
 
-		if i == 5 {
-			fmt.Printf(" multiplicamos por 2 \n")
-			i *= 2
-			continue // Manda el control al inicio del for.
-		}
-		fmt.Printf("     Paso por aqui \n")
-
-		i++
-	}*/
-
-	var i int = 0
+	i = 0
 RUTINA:
 	for i < 10 {
 		if i == 4 {
